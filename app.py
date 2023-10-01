a6da9b0f9e1b0f519c7b7bf430c74ff8f0e2c203
youjunchao <2835190741@qq.com>
2023-10-01 15:10:09 +0800

    🐶

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QAction, QGraphicsView, QGraphicsScene, QSizePolicy, QFrame, QGraphicsPixmapItem, \
-    QPushButton
+    QPushButton, QHBoxLayout, QWidget, QVBoxLayout, QGroupBox
 
 from interactive_demo.controller import InteractiveController
 from interactive_demo.wrappers import FocusLabelFrame
@@ -41,11 +41,11 @@ class InteractiveDemoApp(QtWidgets.QMainWindow):
         self._init_state()
         self._add_menu()
         self._add_canvas()
-        # self._add_buttons()
-
+        self._add_buttons()
+        print("test")
         # def _update_image(self, reset_canvas=False)方法添加的
         self.image_item = QGraphicsPixmapItem()
-        self.canvas_scene.addItem(self.image_item)
+        # self.canvas_scene.addItem(self.image_item)
 
     # 初始化应用程序的状态，包括一些布尔值、整数值、双精度浮点数以及字符串值的变量。
     # 这些变量似乎用于跟踪和控制应用程序的行为和用户界面的不同方面
@@ -108,72 +108,136 @@ class InteractiveDemoApp(QtWidgets.QMainWindow):
 
     # 建一个包含图像显示的画布，并将其添加到用户界面中。
     def _add_canvas(self):
-        self.canvas_frame = QFrame(self.centralWidget())  # 使用 central_widget 作为父控件
-        self.canvas_frame.setObjectName("canvas_frame")
-        self.canvas_frame.setStyleSheet("border: 1px solid black;")
+        # 这是整个窗口的布局
+        self.setWindowTitle("Interactive Demo App")
+        self.setGeometry(10, 10, 1000, 1000)
 
-        self.canvas_view = QGraphicsView(self.canvas_frame)
-        self.canvas_view.setObjectName("canvas_view")
-        self.canvas_view.setStyleSheet("background-color: white;")
-        self.canvas_view.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-        self.canvas_scene = QGraphicsScene(self.canvas_frame)
-        self.canvas_view.setScene(self.canvas_scene)
+        # Canvas Frame
+        self.canvas_frame = QtWidgets.QGroupBox("Image", self)
 
-        self.canvas_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        # 左上角的位置是(30距离屏幕左边的距离, 50距离屏幕顶部的距离)，并且窗口的宽度为780像素，高度为500像素
+        self.canvas_frame.setGeometry(30, 50, 180, 400)
+
+        self.canvas_frame_layout = QtWidgets.QGridLayout(self.canvas_frame)
+        self.canvas_frame_layout.setContentsMargins(5, 5, 5, 5)
+
+        self.canvas = QtWidgets.QGraphicsView(self)
+        self.canvas.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.canvas.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.canvas_frame_layout.addWidget(self.canvas, 0, 0)
+        self.canvas_frame.setLayout(self.canvas_frame_layout)
 
         self.image_on_canvas = None
 
+        self.show()
+
     def _add_buttons(self):
-        self.control_frame = FocusLabelFrame(self)
-        label = QtWidgets.QLabel("Controls")
-        self.control_frame_layout = QtWidgets.QVBoxLayout(self.control_frame)
-        self.control_frame_layout.addWidget(label)
-        self.control_frame_layout.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTop)
+        # 展示control_frame，在整窗口个中和canvas_frame水平并列
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        horizontal_layout = QHBoxLayout()
+        central_widget.setLayout(horizontal_layout)
+
+        self.control_frame = QtWidgets.QGroupBox("Controls", self)
+        self.control_frame.setGeometry(250, 50, 300, 400)
+
+        horizontal_layout.addWidget(self.canvas_frame)
+        horizontal_layout.addWidget(self.control_frame)
+
+        self.control_frame_layout = QtWidgets.QGridLayout(self.control_frame)
         self.control_frame_layout.setContentsMargins(5, 5, 5, 5)
 
-        clicks_options_frame = QtWidgets.QGroupBox("Clicks management")
+        # 展示clicks_options_frame，在control_frame中垂直展示
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        self.finish_object_button = QtWidgets.QPushButton('Finish\nobject')
-        self.finish_object_button.setStyleSheet('background-color: #b6d7a8; color: black;')
-        self.finish_object_button.setFixedSize(100, 40)
-        self.finish_object_button.setDisabled(True)
-        self.finish_object_button.clicked.connect(self.controller.finish_object)
+        horizontal_layout = QVBoxLayout()
+        central_widget.setLayout(horizontal_layout)
 
-        self.undo_click_button = QtWidgets.QPushButton('Undo click')
-        self.undo_click_button.setStyleSheet('background-color: #ffe599; color: black;')
-        self.undo_click_button.setFixedSize(100, 40)
-        self.undo_click_button.setDisabled(True)
-        self.undo_click_button.clicked.connect(self.controller.undo_click)
+        self.control_frame = QGroupBox("Controls", central_widget)
+        horizontal_layout.addWidget(self.control_frame)
 
-        self.reset_clicks_button = QtWidgets.QPushButton('Reset clicks')
-        self.reset_clicks_button.setStyleSheet('background-color: #ea9999; color: black;')
-        self.reset_clicks_button.setFixedSize(100, 40)
-        self.reset_clicks_button.setDisabled(True)
-        self.reset_clicks_button.clicked.connect(self._reset_last_object)
-
-        self.clicks_layout = QtWidgets.QHBoxLayout()
-        self.clicks_layout.setContentsMargins(5, 5, 5, 5)
-        self.clicks_layout.addWidget(self.finish_object_button)
-        self.clicks_layout.addWidget(self.undo_click_button)
-        self.clicks_layout.addWidget(self.reset_clicks_button)
-
-        clicks_options_frame.setLayout(self.clicks_layout)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(clicks_options_frame)
-
-        clicks_widget = QtWidgets.QWidget()
-        clicks_widget.setLayout(self.clicks_layout)
-
-        # Add the QWidget to the QGroupBox
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(clicks_widget)
-        clicks_options_frame.setLayout(layout)
-
-        # Add the QGroupBox to the control_frame
-        control_layout = QtWidgets.QVBoxLayout()
-        control_layout.addWidget(clicks_options_frame)
+        control_layout = QVBoxLayout(self.control_frame)
         self.control_frame.setLayout(control_layout)
+
+        # Clicks Management
+        self.clicks_options_frame = QGroupBox("Clicks management", self.control_frame)
+        control_layout.addWidget(self.clicks_options_frame)
+
+        # self.finish_object_button = QtWidgets.QPushButton("Finish\nobject", self.clicks_options_frame)
+        # self.finish_object_button.setGeometry(1500, 20, 70, 40)
+        # self.finish_object_button.setEnabled(False)
+        #
+        # self.undo_click_button = QtWidgets.QPushButton("Undo click", self.clicks_options_frame)
+        # self.undo_click_button.setGeometry(1500, 20, 70, 40)
+        # self.undo_click_button.setEnabled(False)
+
+        self.show()
+
+        # self.reset_clicks_button = QtWidgets.QPushButton("Reset clicks", self.clicks_options_frame)
+        # self.reset_clicks_button.setGeometry(170, 20, 70, 40)
+        # self.reset_clicks_button.setEnabled(False)
+        #
+        # # ZoomIn Options
+        # self.zoomin_options_frame = QtWidgets.QGroupBox("ZoomIn options", self.control_frame)
+        # self.zoomin_options_frame.setGeometry(270, 20, 250, 70)
+        #
+        # self.use_zoomin_checkbox = QtWidgets.QCheckBox("Use ZoomIn", self.zoomin_options_frame)
+        # self.use_zoomin_checkbox.setGeometry(10, 20, 150, 20)
+        #
+        # self.fixed_crop_checkbox = QtWidgets.QCheckBox("Fixed crop", self.zoomin_options_frame)
+        # self.fixed_crop_checkbox.setGeometry(10, 50, 150, 20)
+        #
+        # # BRS Options
+        # self.brs_options_frame = QtWidgets.QGroupBox("BRS options", self.control_frame)
+        # self.brs_options_frame.setGeometry(530, 20, 250, 70)
+        #
+        # self.brs_mode_combo = QtWidgets.QComboBox(self.brs_options_frame)
+        # self.brs_mode_combo.setGeometry(10, 20, 100, 30)
+        # self.brs_mode_combo.addItems(["Mode 1", "Mode 2", "Mode 3"])  # Replace with your modes
+        #
+        # self.network_clicks_label = QtWidgets.QLabel("Network clicks", self.brs_options_frame)
+        # self.network_clicks_label.setGeometry(120, 20, 100, 30)
+        #
+        # self.network_clicks_spinbox = QtWidgets.QSpinBox(self.brs_options_frame)
+        # self.network_clicks_spinbox.setGeometry(230, 20, 60, 30)
+        #
+        # # Other Options (Threshold, Alpha Blend, Click Radius)
+        # # Add similar code for these options...
+        #
+        # # Predictions Threshold
+        # self.prob_thresh_frame = QtWidgets.QGroupBox("Predictions threshold", self.control_frame)
+        # self.prob_thresh_frame.setGeometry(10, 100, 250, 70)
+        #
+        # self.prob_thresh_slider = QtWidgets.QSlider(Qt.Horizontal, self.prob_thresh_frame)
+        # self.prob_thresh_slider.setGeometry(10, 20, 230, 30)
+        # self.prob_thresh_slider.setRange(0, 100)
+        # self.prob_thresh_slider.setValue(50)
+        # # self.prob_thresh_slider.valueChanged.connect(self._update_prob_thresh)
+        #
+        # # Alpha Blending Coefficient
+        # self.alpha_blend_frame = QtWidgets.QGroupBox("Alpha blending coefficient", self.control_frame)
+        # self.alpha_blend_frame.setGeometry(270, 100, 250, 70)
+        #
+        # self.alpha_blend_slider = QtWidgets.QSlider(Qt.Horizontal, self.alpha_blend_frame)
+        # self.alpha_blend_slider.setGeometry(10, 20, 230, 30)
+        # self.alpha_blend_slider.setRange(0, 100)
+        # self.alpha_blend_slider.setValue(50)
+        # # self.alpha_blend_slider.valueChanged.connect(self._update_blend_alpha)
+        #
+        # # Visualisation Click Radius
+        # self.click_radius_frame = QtWidgets.QGroupBox("Visualisation click radius", self.control_frame)
+        # self.click_radius_frame.setGeometry(530, 100, 250, 70)
+        #
+        # self.click_radius_slider = QtWidgets.QSlider(Qt.Horizontal, self.click_radius_frame)
+        # self.click_radius_slider.setGeometry(10, 20, 230, 30)
+        # self.click_radius_slider.setRange(0, 7)
+        # self.click_radius_slider.setValue(3)
+        # # self.click_radius_slider.valueChanged.connect(self._update_click_radius)
+
+        self.show()
 
     def _load_image_callback(self):
         # 打开一个文件对话框，允许用户选择图像文件。用户可以在文件对话框中浏览文件系统，
