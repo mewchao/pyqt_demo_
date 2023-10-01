25402be2c24cc67b9989392554bff6ce0bc08029
youjunchao <2835190741@qq.com>
2023-10-01 23:58:33 +0800

    布局基本完成

--- app.py
+++ app.py
@@ -40,8 +40,10 @@ class InteractiveDemoApp(QtWidgets.QMainWindow):
 
         self._init_state()
         self._add_menu()
+        self._add_window()
         self._add_canvas()
         self._add_buttons()
+        self.show()
         print("test")
         # def _update_image(self, reset_canvas=False)方法添加的
         self.image_item = QGraphicsPixmapItem()
@@ -106,138 +108,150 @@ class InteractiveDemoApp(QtWidgets.QMainWindow):
         exit_action.triggered.connect(self.close)
         menubar.addAction(exit_action)
 
+    # 这是整个窗口的布局
+    def _add_window(self):
+        self.setWindowTitle("Interactive Demo App")
+        self.setGeometry(0, 0, 1300, 1000)
+
+        # 创建一个名为central_widget的QWidget，设置中央部件的大小为800x600像素 它将作为主窗口的中央部件，也就是主要的可见区域，设置为主窗口的中央部件
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setFixedSize(1000, 1000)
+
     # 建一个包含图像显示的画布，并将其添加到用户界面中。
     def _add_canvas(self):
-        # 这是整个窗口的布局
-        self.setWindowTitle("Interactive Demo App")
-        self.setGeometry(10, 10, 1000, 1000)
 
-        # Canvas Frame
+        # 画布控件
         self.canvas_frame = QtWidgets.QGroupBox("Image", self)
-
-        # 左上角的位置是(30距离屏幕左边的距离, 50距离屏幕顶部的距离)，并且窗口的宽度为780像素，高度为500像素
-        self.canvas_frame.setGeometry(30, 50, 180, 400)
 
         self.canvas_frame_layout = QtWidgets.QGridLayout(self.canvas_frame)
         self.canvas_frame_layout.setContentsMargins(5, 5, 5, 5)
 
         self.canvas = QtWidgets.QGraphicsView(self)
-        self.canvas.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.canvas.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        # 取消禁用了滚动
+        self.canvas.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.canvas.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
+        # 将canvas添加到canvas_frame_layout的第0行、第0列的位置
         self.canvas_frame_layout.addWidget(self.canvas, 0, 0)
+        # canvas_frame中的所有小部件将按照canvas_frame_layout定义的规则进行排列和布局。
         self.canvas_frame.setLayout(self.canvas_frame_layout)
 
         self.image_on_canvas = None
 
-        self.show()
-
     def _add_buttons(self):
-        # 展示control_frame，在整窗口个中和canvas_frame水平并列
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
-        horizontal_layout = QHBoxLayout()
-        central_widget.setLayout(horizontal_layout)
-
+        # 创建控制台部件
         self.control_frame = QtWidgets.QGroupBox("Controls", self)
-        self.control_frame.setGeometry(250, 50, 300, 400)
+        self.control_frame.setFixedSize(400, 1000)
 
-        horizontal_layout.addWidget(self.canvas_frame)
-        horizontal_layout.addWidget(self.control_frame)
+        # 水平布局管理器 设置为 central_widget 的布局管理器
+        horizontal_layout_H = QHBoxLayout()
+        self.central_widget.setLayout(horizontal_layout_H)
+
+        horizontal_layout_H.addWidget(self.canvas_frame)
+        horizontal_layout_H.addWidget(self.control_frame)
 
         self.control_frame_layout = QtWidgets.QGridLayout(self.control_frame)
+        # 左上右下
         self.control_frame_layout.setContentsMargins(5, 5, 5, 5)
 
-        # 展示clicks_options_frame，在control_frame中垂直展示
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        # 垂直布局管理器 设置为 control_frame 的布局管理器
+        horizontal_layout_V = QVBoxLayout()
+        self.control_frame.setLayout(horizontal_layout_V)
 
-        horizontal_layout = QVBoxLayout()
-        central_widget.setLayout(horizontal_layout)
-
-        self.control_frame = QGroupBox("Controls", central_widget)
-        horizontal_layout.addWidget(self.control_frame)
-
-        control_layout = QVBoxLayout(self.control_frame)
-        self.control_frame.setLayout(control_layout)
-
-        # Clicks Management
+        # control_frame的第一个区域---->点击区域
         self.clicks_options_frame = QGroupBox("Clicks management", self.control_frame)
-        control_layout.addWidget(self.clicks_options_frame)
+        self.clicks_options_frame.setFixedSize(375, 200)
+        self.clicks_options_frame.setGeometry(10, 20, 375, 175)
 
-        # self.finish_object_button = QtWidgets.QPushButton("Finish\nobject", self.clicks_options_frame)
-        # self.finish_object_button.setGeometry(1500, 20, 70, 40)
-        # self.finish_object_button.setEnabled(False)
-        #
-        # self.undo_click_button = QtWidgets.QPushButton("Undo click", self.clicks_options_frame)
-        # self.undo_click_button.setGeometry(1500, 20, 70, 40)
-        # self.undo_click_button.setEnabled(False)
+        # 在布局的顶部插入 clicks_options_frame
+        horizontal_layout_V.insertWidget(0, self.clicks_options_frame)
 
-        self.show()
+        Clickmanagement_shorizontal_layout_H = QHBoxLayout()
+        self.clicks_options_frame.setLayout(Clickmanagement_shorizontal_layout_H)
 
-        # self.reset_clicks_button = QtWidgets.QPushButton("Reset clicks", self.clicks_options_frame)
-        # self.reset_clicks_button.setGeometry(170, 20, 70, 40)
-        # self.reset_clicks_button.setEnabled(False)
-        #
-        # # ZoomIn Options
-        # self.zoomin_options_frame = QtWidgets.QGroupBox("ZoomIn options", self.control_frame)
-        # self.zoomin_options_frame.setGeometry(270, 20, 250, 70)
-        #
-        # self.use_zoomin_checkbox = QtWidgets.QCheckBox("Use ZoomIn", self.zoomin_options_frame)
-        # self.use_zoomin_checkbox.setGeometry(10, 20, 150, 20)
-        #
-        # self.fixed_crop_checkbox = QtWidgets.QCheckBox("Fixed crop", self.zoomin_options_frame)
-        # self.fixed_crop_checkbox.setGeometry(10, 50, 150, 20)
-        #
-        # # BRS Options
-        # self.brs_options_frame = QtWidgets.QGroupBox("BRS options", self.control_frame)
-        # self.brs_options_frame.setGeometry(530, 20, 250, 70)
-        #
-        # self.brs_mode_combo = QtWidgets.QComboBox(self.brs_options_frame)
-        # self.brs_mode_combo.setGeometry(10, 20, 100, 30)
-        # self.brs_mode_combo.addItems(["Mode 1", "Mode 2", "Mode 3"])  # Replace with your modes
-        #
-        # self.network_clicks_label = QtWidgets.QLabel("Network clicks", self.brs_options_frame)
-        # self.network_clicks_label.setGeometry(120, 20, 100, 30)
-        #
-        # self.network_clicks_spinbox = QtWidgets.QSpinBox(self.brs_options_frame)
-        # self.network_clicks_spinbox.setGeometry(230, 20, 60, 30)
-        #
-        # # Other Options (Threshold, Alpha Blend, Click Radius)
-        # # Add similar code for these options...
-        #
-        # # Predictions Threshold
-        # self.prob_thresh_frame = QtWidgets.QGroupBox("Predictions threshold", self.control_frame)
-        # self.prob_thresh_frame.setGeometry(10, 100, 250, 70)
-        #
-        # self.prob_thresh_slider = QtWidgets.QSlider(Qt.Horizontal, self.prob_thresh_frame)
-        # self.prob_thresh_slider.setGeometry(10, 20, 230, 30)
-        # self.prob_thresh_slider.setRange(0, 100)
-        # self.prob_thresh_slider.setValue(50)
-        # # self.prob_thresh_slider.valueChanged.connect(self._update_prob_thresh)
-        #
-        # # Alpha Blending Coefficient
-        # self.alpha_blend_frame = QtWidgets.QGroupBox("Alpha blending coefficient", self.control_frame)
-        # self.alpha_blend_frame.setGeometry(270, 100, 250, 70)
-        #
-        # self.alpha_blend_slider = QtWidgets.QSlider(Qt.Horizontal, self.alpha_blend_frame)
-        # self.alpha_blend_slider.setGeometry(10, 20, 230, 30)
-        # self.alpha_blend_slider.setRange(0, 100)
-        # self.alpha_blend_slider.setValue(50)
-        # # self.alpha_blend_slider.valueChanged.connect(self._update_blend_alpha)
-        #
-        # # Visualisation Click Radius
-        # self.click_radius_frame = QtWidgets.QGroupBox("Visualisation click radius", self.control_frame)
-        # self.click_radius_frame.setGeometry(530, 100, 250, 70)
-        #
-        # self.click_radius_slider = QtWidgets.QSlider(Qt.Horizontal, self.click_radius_frame)
-        # self.click_radius_slider.setGeometry(10, 20, 230, 30)
-        # self.click_radius_slider.setRange(0, 7)
-        # self.click_radius_slider.setValue(3)
-        # # self.click_radius_slider.valueChanged.connect(self._update_click_radius)
+        self.finish_object_button = QtWidgets.QPushButton("Finish\nobject", self.clicks_options_frame)
+        self.finish_object_button.setGeometry(10, 20, 100, 150)
+        self.finish_object_button.setEnabled(False)
 
-        self.show()
+        self.undo_click_button = QtWidgets.QPushButton("Undo click", self.clicks_options_frame)
+        self.undo_click_button.setGeometry(10, 20, 100, 150)
+        self.undo_click_button.setEnabled(False)
+
+        self.reset_clicks_button = QtWidgets.QPushButton("Reset clicks", self.clicks_options_frame)
+        self.reset_clicks_button.setGeometry(10, 20, 100, 150)
+        self.reset_clicks_button.setEnabled(False)
+
+        Clickmanagement_shorizontal_layout_H.insertWidget(0, self.finish_object_button)
+        Clickmanagement_shorizontal_layout_H.insertWidget(1, self.undo_click_button)
+        Clickmanagement_shorizontal_layout_H.insertWidget(2, self.reset_clicks_button)
+
+        # ZoomIn Options
+        self.zoomin_options_frame = QtWidgets.QGroupBox("ZoomIn options", self.control_frame)
+        self.zoomin_options_frame.setFixedSize(375, 200)
+        self.zoomin_options_frame.setGeometry(10, 20+175+35, 375, 175)
+
+        # 在布局的第二个索引插入 zoomin_options_frame
+        horizontal_layout_V.insertWidget(1, self.zoomin_options_frame)
+
+        ZoomInoptions_shorizontal_layout_V = QVBoxLayout()
+        self.zoomin_options_frame.setLayout(ZoomInoptions_shorizontal_layout_V)
+
+        self.use_zoomin_checkbox = QtWidgets.QCheckBox("Use ZoomIn", self.zoomin_options_frame)
+        self.use_zoomin_checkbox.setGeometry(10, 20, 150, 20)
+
+        self.fixed_crop_checkbox = QtWidgets.QCheckBox("Fixed crop", self.zoomin_options_frame)
+        self.fixed_crop_checkbox.setGeometry(10, 50, 150, 20)
+
+        ZoomInoptions_shorizontal_layout_V.insertWidget(0, self.use_zoomin_checkbox)
+        ZoomInoptions_shorizontal_layout_V.insertWidget(1, self.fixed_crop_checkbox)
+
+        # BRS Options
+        self.brs_options_frame = QtWidgets.QGroupBox("BRS options", self.control_frame)
+        self.brs_options_frame.setGeometry(10, 460, 375, 100)
+
+        # 在布局的第三个索引插入 brs_options_frame
+        horizontal_layout_V.insertWidget(2, self.brs_options_frame)
+
+        self.brs_mode_combo = QtWidgets.QComboBox(self.brs_options_frame)
+        self.brs_mode_combo.setGeometry(10, 38, 100, 30)
+        self.brs_mode_combo.addItems(self.brs_modes)
+
+        self.network_clicks_label = QtWidgets.QLabel("Network clicks", self.brs_options_frame)
+        self.network_clicks_label.setGeometry(130, 15, 100, 30)
+
+        self.network_clicks_spinbox = QtWidgets.QSpinBox(self.brs_options_frame)
+        self.network_clicks_spinbox.setGeometry(130, 38, 60, 30)
+
+        # Predictions Threshold
+        self.prob_thresh_frame = QtWidgets.QGroupBox("Predictions threshold", self.control_frame)
+        self.prob_thresh_frame.setGeometry(10, 460+110, 375, 100)
+
+        self.prob_thresh_slider = QtWidgets.QSlider(Qt.Horizontal, self.prob_thresh_frame)
+        self.prob_thresh_slider.setGeometry(10, 30, 350, 30)
+        self.prob_thresh_slider.setRange(0, 100)
+        self.prob_thresh_slider.setValue(50)
+        # self.prob_thresh_slider.valueChanged.connect(self._update_prob_thresh)
+
+        # Alpha Blending Coefficient
+        self.alpha_blend_frame = QtWidgets.QGroupBox("Alpha blending coefficient", self.control_frame)
+        self.alpha_blend_frame.setGeometry(10, 460+110+110, 375, 100)
+
+        self.alpha_blend_slider = QtWidgets.QSlider(Qt.Horizontal, self.alpha_blend_frame)
+        self.alpha_blend_slider.setGeometry(10, 30, 350, 30)
+        self.alpha_blend_slider.setRange(0, 100)
+        self.alpha_blend_slider.setValue(50)
+        # self.alpha_blend_slider.valueChanged.connect(self._update_blend_alpha)
+
+        # Visualisation Click Radius
+        self.click_radius_frame = QtWidgets.QGroupBox("Visualisation click radius", self.control_frame)
+        self.click_radius_frame.setGeometry(10, 460+110+110+110, 375, 100)
+
+        self.click_radius_slider = QtWidgets.QSlider(Qt.Horizontal, self.click_radius_frame)
+        self.click_radius_slider.setGeometry(10, 30, 350, 30)
+        self.click_radius_slider.setRange(0, 7)
+        self.click_radius_slider.setValue(3)
+        # self.click_radius_slider.valueChanged.connect(self._update_click_radius)
 
     def _load_image_callback(self):
         # 打开一个文件对话框，允许用户选择图像文件。用户可以在文件对话框中浏览文件系统，
